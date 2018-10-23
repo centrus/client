51956b8522bb87c02effe7815468a91bf81da5ba
centrus <office@centrus.rs>
2018-10-24 01:36:49 +0200

    Minor changes

--- routes/web.php
+++ routes/web.php
@@ -18,25 +18,25 @@ Route::get('/', function () {
 });
 
 
-Route::get('/redirect',function(){
+Route::get('/login',function(){
    $query = http_build_query([
-        'client_id' => 5,
+        'client_id' => 4,
         'redirect_uri' => "http://localhost/client/public/callback",
         'response_type' => 'code',
         'scope' => '',
     ]);
 
-    return redirect('http://passport.dev/oauth/authorize?'.$query);
+    return redirect('http://passport.centrus.local/oauth/authorize?'.$query);
 });
 
 Route::get('/callback', function(Request $request) {
    $http = new GuzzleHttp\Client;
    //dd($request);
-    $response = $http->post('http://passport.dev/oauth/token', [
+    $response = $http->post('http://passport.centrus.local/oauth/token', [
         'form_params' => [
             'grant_type' => 'authorization_code',
-            'client_id' => 5,
-            'client_secret' => '1crEkNIDwF5g0ntcP5QFIs8JbK37pgtHLXSgzE3y',
+            'client_id' => 4,
+            'client_secret' => 'KtkrR0mV0kYQ3GAz3NFkvcO9lqTbA2ip1B8FXDO9',
             'redirect_uri' => "http://localhost/client/public/callback",
             'code' => $request->code,
         ],
